fd2a8c6de0fe1883ac1ada23635dd3acf5414e97
kellyannmcnamara01 <kellyannmcnamara01@gmail.com>
2017-05-23 19:09:33 -0400

    added trailer section on product page

--- equipment.php
+++ equipment.php
@@ -104,7 +104,106 @@ include "head.php";
     </div>
     <div class="clear"></div>
 </div>
-
+<!-- ######## -->
+<div id="trailers">
+    <h2>Trailers</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu.
+    </p>
+</div>
+<div id="trailers-items">
+    <div class="col-3">
+        <img src="opt-imgs/for-sale-01.jpg">
+        <div class="item-details">
+            <svg height="15" width="46" class="items-arrow-odd">
+                <polygon points="23,0 0,15 46,15" style="fill:#fff" />
+            </svg>
+            <h3>Equipment Title Placement</h3>
+            <p>Make Company</p>
+            <p>Model of Vehicle</p>
+            <p>Year 2000</p>
+            <p>10,000 Miles</p>
+            <p>Other Stat</p>
+            <p>Other Stat</p>
+        </div>
+    </div>
+    <div class="col-3">
+        <div class="item-details">
+            <h3>Equipment Title Placement</h3>
+            <p>Make Company</p>
+            <p>Model of Vehicle</p>
+            <p>Year 2000</p>
+            <p>10,000 Miles</p>
+            <p>Other Stat</p>
+            <p>Other Stat</p>
+            <svg height="15" width="46" class="items-arrow-even">
+                <polygon points="23,0 0,15 46,15" style="fill:#fff" />
+            </svg>
+        </div>
+        <img src="opt-imgs/for-sale-01.jpg">
+    </div>
+    <div class="col-3">
+        <img src="opt-imgs/for-sale-01.jpg">
+        <div class="item-details">
+            <svg height="15" width="46" class="items-arrow-odd">
+                <polygon points="23,0 0,15 46,15" style="fill:#fff" />
+            </svg>
+            <h3>Equipment Title Placement</h3>
+            <p>Make Company</p>
+            <p>Model of Vehicle</p>
+            <p>Year 2000</p>
+            <p>10,000 Miles</p>
+            <p>Other Stat</p>
+            <p>Other Stat</p>
+        </div>
+    </div>
+    <div class="col-3">
+        <img src="opt-imgs/for-sale-01.jpg">
+        <div class="item-details">
+            <svg height="15" width="46" class="items-arrow-odd">
+                <polygon points="23,0 0,15 46,15" style="fill:#fff" />
+            </svg>
+            <h3>Equipment Title Placement</h3>
+            <p>Make Company</p>
+            <p>Model of Vehicle</p>
+            <p>Year 2000</p>
+            <p>10,000 Miles</p>
+            <p>Other Stat</p>
+            <p>Other Stat</p>
+        </div>
+    </div>
+    <div class="col-3">
+        <div class="item-details">
+            <h3>Equipment Title Placement</h3>
+            <p>Make Company</p>
+            <p>Model of Vehicle</p>
+            <p>Year 2000</p>
+            <p>10,000 Miles</p>
+            <p>Other Stat</p>
+            <p>Other Stat</p>
+            <svg height="15" width="46" class="items-arrow-even">
+                <polygon points="23,0 0,15 46,15" style="fill:#fff" />
+            </svg>
+        </div>
+        <img src="opt-imgs/for-sale-01.jpg">
+    </div>
+    <div class="col-3">
+        <img src="opt-imgs/for-sale-01.jpg">
+        <div class="item-details">
+            <svg height="15" width="46" class="items-arrow-odd">
+                <polygon points="23,0 0,15 46,15" style="fill:#fff" />
+            </svg>
+            <h3>Equipment Title Placement</h3>
+            <p>Make Company</p>
+            <p>Model of Vehicle</p>
+            <p>Year 2000</p>
+            <p>10,000 Miles</p>
+            <p>Other Stat</p>
+            <p>Other Stat</p>
+        </div>
+    </div>
+    <div class="clear"></div>
+</div>
 <?php
 
 //include footer
