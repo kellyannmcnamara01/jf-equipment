b6bcb9cd0c847ee0f05d0232c2d3f6320ff1416e
kellyannmcnamara01 <kellyannmcnamara01@gmail.com>
2017-05-23 17:42:06 -0400

    set up basic equipment sales section

--- equipment.php
+++ equipment.php
@@ -5,7 +5,93 @@ include "head.php";
 
 ?>
 
-<h1>hi</h1>
+<div id="construction">
+    <h2>Construction Equipment</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu.
+    </p>
+</div>
+<div id="construction-items">
+    <div class="col-3">
+        <img src="">
+        <svg height="15" width="46" class="items-arrow">
+            <polygon points="23,0 0,15 46,15" style="fill:#ff0" />
+        </svg>
+        <h3>Equipment Title Placement</h3>
+        <p>Make Company</p>
+        <p>Model of Vehicle</p>
+        <p>Year 2000</p>
+        <p>10,000 Miles</p>
+        <p>Other Stat</p>
+        <p>Other Stat</p>
+    </div>
+    <div class="col-3">
+        <img src="">
+        <svg height="15" width="46" class="items-arrow">
+            <polygon points="23,0 0,15 46,15" style="fill:#ff0" />
+        </svg>
+        <h3>Equipment Title Placement</h3>
+        <p>Make Company</p>
+        <p>Model of Vehicle</p>
+        <p>Year 2000</p>
+        <p>10,000 Miles</p>
+        <p>Other Stat</p>
+        <p>Other Stat</p>
+    </div>
+    <div class="col-3">
+        <img src="">
+        <svg height="15" width="46" class="items-arrow">
+            <polygon points="23,0 0,15 46,15" style="fill:#ff0" />
+        </svg>
+        <h3>Equipment Title Placement</h3>
+        <p>Make Company</p>
+        <p>Model of Vehicle</p>
+        <p>Year 2000</p>
+        <p>10,000 Miles</p>
+        <p>Other Stat</p>
+        <p>Other Stat</p>
+    </div>
+    <div class="col-3">
+        <img src="">
+        <svg height="15" width="46" class="items-arrow">
+            <polygon points="23,0 0,15 46,15" style="fill:#ff0" />
+        </svg>
+        <h3>Equipment Title Placement</h3>
+        <p>Make Company</p>
+        <p>Model of Vehicle</p>
+        <p>Year 2000</p>
+        <p>10,000 Miles</p>
+        <p>Other Stat</p>
+        <p>Other Stat</p>
+    </div>
+    <div class="col-3">
+        <img src="">
+        <svg height="15" width="46" class="items-arrow">
+            <polygon points="23,0 0,15 46,15" style="fill:#ff0" />
+        </svg>
+        <h3>Equipment Title Placement</h3>
+        <p>Make Company</p>
+        <p>Model of Vehicle</p>
+        <p>Year 2000</p>
+        <p>10,000 Miles</p>
+        <p>Other Stat</p>
+        <p>Other Stat</p>
+    </div>
+    <div class="col-3">
+        <img src="">
+        <svg height="15" width="46" class="items-arrow">
+            <polygon points="23,0 0,15 46,15" style="fill:#ff0" />
+        </svg>
+        <h3>Equipment Title Placement</h3>
+        <p>Make Company</p>
+        <p>Model of Vehicle</p>
+        <p>Year 2000</p>
+        <p>10,000 Miles</p>
+        <p>Other Stat</p>
+        <p>Other Stat</p>
+    </div>
+    <div class="clear"></div>
+</div>
 
 <?php
 
