99177abf1a113f4d27b5c55e59567a0b702b08ae
kellyannmcnamara01 <kellyannmcnamara01@gmail.com>
2017-05-23 13:15:33 -0400

    create a footer and header php file and link them to the index.php file

---
+++ footer.php
@@ -0,0 +1,48 @@
+<?php
+?>
+
+<footer>
+    <div id="footer-content">
+        <div id="contact-info">
+            <h3>Our Information</h3>
+            <p><a href="mailto:">info@jfequipmentsales.com</a></p>
+            <p><a href="tel:">000.000.0000</a></p>
+            <p><a href="" target="_blank">6265 Las Alturas Dr.</a></p>
+            <p><a href="" target="_blank">Las Cruces NM.</a></p>
+            <p><a href="" target="_blank">88011</a></p>
+        </div>
+        <div id="contact-form">
+            <h3>Contact Us</h3>
+            <form method="get" action="index.php">
+                <div class="col-2">
+                    <div>
+                        <label for="name">Full Name</label>
+                        <input type="text" id="name" name="name" placeholder="Full Name">
+                    </div>
+                    <div>
+                        <label for="email">Email</label>
+                        <input type="text" id="email" name="email" placeholder="Email">
+                    </div>
+                    <div>
+                        <label for="number">Phone Number</label>
+                        <input type="text" id="number" name="number" placeholder="Phone Number">
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div>
+                        <label for="msg">Message</label>
+                        <textarea id="msg" name="msg" placeholder="Message"></textarea>
+                    </div>
+                    <div>
+                        <input type="submit" id="submit" name="submit">
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </form>
+        </div>
+        <div class="clear"></div>
+    </div>
+</footer>
+</main>
+</body>
+</html>
---
+++ head.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>J&amp;F Equipment Sales</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,900" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<aside id="sidebar">
+    <div id="nav-logo">
+        <img src="opt-imgs/jf-logo.svg" width="100">
+        <h1>J&amp;F Equipment Sales</h1>
+    </div>
+    <nav>
+        <div id="nav-links">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Used Equipment</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+            <div id="nav-social">
+                <a href="" target="_blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5.05 10.83" id="fb"><defs><style>.icon-fill{fill:#f1f3f4;}</style></defs><title>Asset 1</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="icon-fill" d="M1.12,10.83H3.36V5.42H4.85L5,3.55H3.36V2.62c0-.49,0-.75.74-.75H5V0H3.54A2.11,2.11,0,0,0,1.12,2.43V3.55H0V5.42H1.12Z"/></g></g></svg>
+                </a>
+                <a href="" target="_blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9.83 9.83"><defs><style>.icon-fill{fill:#f1f3f4;}</style></defs><title>Asset 3</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="icon-fill" d="M8.6,0H1.23A1.21,1.21,0,0,0,0,1.23V8.6A1.21,1.21,0,0,0,1.23,9.83H8.6A1.21,1.21,0,0,0,9.83,8.6V1.23A1.21,1.21,0,0,0,8.6,0ZM4.92,3a1.94,1.94,0,0,1,2,1.91,1.94,1.94,0,0,1-2,1.91,1.94,1.94,0,0,1-2-1.91A1.94,1.94,0,0,1,4.92,3ZM8.6,8.29c0,.22-.09.31-.31.31H1.54c-.21,0-.31-.09-.31-.31V4L2,4.16A2.86,2.86,0,0,0,1.87,5,3,3,0,0,0,4.92,8,3,3,0,0,0,8,5a2.86,2.86,0,0,0-.13-.84L8.6,4v4.3Zm0-5.53a.31.31,0,0,1-.31.31H7.06a.31.31,0,0,1-.31-.31V1.54a.31.31,0,0,1,.31-.31H8.29a.31.31,0,0,1,.31.31Z"/></g></g></svg>
+                </a>
+                <a href="" target="_blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10.71 8.87"><defs><style>.icon-fill{fill:#f1f3f4;}</style></defs><title>Asset 2</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="icon-fill" d="M6.71.1A2.24,2.24,0,0,0,5.2,2.36l0,.38-.39,0A6.29,6.29,0,0,1,1.15.88L.64.37.51.75A2.26,2.26,0,0,0,1,3.06c.31.33.24.38-.29.18a.9.9,0,0,0-.36-.09,2.81,2.81,0,0,0,.28,1,2.5,2.5,0,0,0,1.06,1l.38.18H1.6c-.43,0-.45,0-.4.17A2.34,2.34,0,0,0,2.65,6.85L3.13,7l-.42.25a4.33,4.33,0,0,1-2.07.58A2.29,2.29,0,0,0,0,7.91a5.93,5.93,0,0,0,1.49.69A6.53,6.53,0,0,0,6.57,8,6.8,6.8,0,0,0,9.15,5a8.07,8.07,0,0,0,.53-2.35c0-.36,0-.41.46-.84a5.21,5.21,0,0,0,.54-.6c.08-.15.07-.15-.32,0-.66.23-.75.2-.43-.15a2.28,2.28,0,0,0,.53-.84s-.12,0-.25.09a4.25,4.25,0,0,1-.68.27L9.1.68,8.72.42A3.16,3.16,0,0,0,8.07.07,2.64,2.64,0,0,0,6.71.1Z"/></g></g></svg>
+                </a>
+            </div>
+        </div>
+    </nav>
+</aside>
+<main>
