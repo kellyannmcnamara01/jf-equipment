6ff8992560747a0a158c39acf63a6cfb2df7dae0
kellyannmcnamara01 <kellyannmcnamara01@gmail.com>
2017-05-23 18:05:16 -0400

    start to styling the items for sale first col

--- equipment.php
+++ equipment.php
@@ -13,17 +13,19 @@ include "head.php";
 </div>
 <div id="construction-items">
     <div class="col-3">
-        <img src="">
+        <img src="opt-imgs/for-sale-01.jpg">
         <svg height="15" width="46" class="items-arrow">
-            <polygon points="23,0 0,15 46,15" style="fill:#ff0" />
+            <polygon points="23,0 0,15 46,15" style="fill:#00f" />
         </svg>
-        <h3>Equipment Title Placement</h3>
-        <p>Make Company</p>
-        <p>Model of Vehicle</p>
-        <p>Year 2000</p>
-        <p>10,000 Miles</p>
-        <p>Other Stat</p>
-        <p>Other Stat</p>
+        <div class="item-details">
+            <h3>Equipment Title Placement</h3>
+            <p>Make Company</p>
+            <p>Model of Vehicle</p>
+            <p>Year 2000</p>
+            <p>10,000 Miles</p>
+            <p>Other Stat</p>
+            <p>Other Stat</p>
+        </div>
     </div>
     <div class="col-3">
         <img src="">
