e3da5c33528f77a233bdfa03c3cb444ec29beaeb
kellyannmcnamara01 <kellyannmcnamara01@gmail.com>
2017-05-23 14:10:18 -0400

    styling for the background imgs and over all placement within the about page

--- about.php
+++ about.php
@@ -6,22 +6,28 @@ include "head.php";
 ?>
 
 <div id="family-owned">
-    <h2>family owned</h2>
-    <p>
-        J & F Equipment sales is a family owned and operated business. Established in 2014 in Las Cruces. We moved to the area in the same year, from where we have been providing our personal customer service ever since, not only to the business community, but to various manufacturing customers, and tradespeople.
-    </p>
+    <div class="about-content">
+        <h2>family owned</h2>
+        <p>
+            J & F Equipment sales is a family owned and operated business. Established in 2014 in Las Cruces. We moved to the area in the same year, from where we have been providing our personal customer service ever since, not only to the business community, but to various manufacturing customers, and tradespeople.
+        </p>
+    </div>
 </div>
 <div id="personal">
-    <h2>one-on-one personal</h2>
-    <p>
-        J & F Equipment sales specializes in one-on-one personal and knowledgeable service. The customer is served by the owner so that pricing decisions and deals can be negotiated instantly on a one-to-one basis. Due to the maintenance of our low overhead costs over the years, J & F Equipment sales has been able to maintain it’s “best price in the business” goal.
-    </p>
+    <div class="about-content">
+        <h2>one-on-one personal</h2>
+        <p>
+            J & F Equipment sales specializes in one-on-one personal and knowledgeable service. The customer is served by the owner so that pricing decisions and deals can be negotiated instantly on a one-to-one basis. Due to the maintenance of our low overhead costs over the years, J & F Equipment sales has been able to maintain it’s “best price in the business” goal.
+        </p>
+    </div>
 </div>
 <div id="customer">
-    <h2>pleasing the customer</h2>
-    <p>
-        J & F Equipment sales is proud to have maintained a reputation over the the past few years; for our personal service, great prices and flexibility when it comes to pleasing the customer. Our prime source of advertising is based on word of mouth which, as always, speaks for itself!
-    </p>
+    <div class="about-content">
+        <h2>pleasing the customer</h2>
+        <p>
+            J & F Equipment sales is proud to have maintained a reputation over the the past few years; for our personal service, great prices and flexibility when it comes to pleasing the customer. Our prime source of advertising is based on word of mouth which, as always, speaks for itself!
+        </p>
+    </div>
 </div>
 
 <?php
